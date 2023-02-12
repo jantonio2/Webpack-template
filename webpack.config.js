e0d9638d8e3698f40043a87daab281c36dba4205
jantonio2 <jesusrochavedia@gmail.com>
2023-02-11 23:54:57 -0400

    Install htmlwebpackplugin and html-loader also configure the webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,31 @@
+const HtmlWebpack = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'development',
 
+  output: {
+    clean: true
+  },
+
   module: {
-    rules: []
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          sources: false
+        }
+      }
+    ]
   },
 
   optimization: {},
 
-  plugins: []
+  plugins: [
+    new HtmlWebpack({
+      title: 'Mi Webpack App',
+      // filename: 'index.html' optional
+      template: './src/index.html'
+    })
+  ]
 }
